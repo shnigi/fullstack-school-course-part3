df396edaf7e6f5aa1e4cb95b63015af488976299
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-02 15:45:29 +0200

    eslint fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const mongoService = require('./models/tietokanta');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const mongoService = require('./models/tietokanta')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 // app.use(morgan('tiny'));
 app.use(express.static('build'))
 
@@ -17,8 +17,8 @@ morgan.token('body', function (req) {
 app.use(morgan(':method :url :body - :response-time ms'))
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+  res.send('<h1>Hello World!</h1>')
+})
 
 const formatPerson = (person) => {
   return {
@@ -29,29 +29,29 @@ const formatPerson = (person) => {
 }
 
 app.get('/api/persons', (req, res) => {
-    mongoService
-    .find({}, {__v: 0})
+  mongoService
+    .find({}, { __v: 0 })
     .then(result => {
-      res.json(result.map(formatPerson));
-      })
-});
+      res.json(result.map(formatPerson))
+    })
+})
 
 app.get('/api/persons/:id', (req, res) => {
   mongoService
-     .findById(req.params.id, {__v: 0})
-     .then(result => {
-       res.json(formatPerson(result));
-     })
-     .catch(error => {
-       res.status(400).send({ error: 'malformatted id' })
-     })
-});
+    .findById(req.params.id, { __v: 0 })
+    .then(result => {
+      res.json(formatPerson(result))
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
+})
 
 app.delete('/api/persons/:id', (req, res) => {
   mongoService
-    .findByIdAndRemove(req.params.id, {__v: 0})
+    .findByIdAndRemove(req.params.id, { __v: 0 })
     .then(result => {
-      res.status(204).end();
+      res.status(204).end()
     })
     .catch(error => {
       res.status(400).send({ error: 'malformatted id' })
@@ -60,21 +60,21 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.put('/api/persons/:id', (req, res) => {
   mongoService
-     .findByIdAndUpdate(req.params.id, req.body, { new: true } )
-     .then(updatedPerson => {
-       res.json(formatPerson(updatedPerson))
-     })
-     .catch(error => {
-       console.log(error)
-       res.status(400).send({ error: 'malformatted id' })
-     })
+    .findByIdAndUpdate(req.params.id, req.body, { new: true } )
+    .then(updatedPerson => {
+      res.json(formatPerson(updatedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const nameExists = (data) => {
-  const name = data.name;
-  const person = persons.find(person => person.name === name);
+  const name = data.name
+  const person = persons.find(person => person.name === name)
   if (person) {
-    return true;
+    return true
   }
 }
 
@@ -82,10 +82,10 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (body === undefined) {
-    return res.status(400).json({error: 'content missing'})
+    return res.status(400).json({ error: 'content missing' })
   }
   if (!body.name || !body.number) {
-    return res.status(400).json({error: 'Name or number missing'})
+    return res.status(400).json({ error: 'Name or number missing' })
   }
   // if (nameExists(body)) {
   //   return res.status(400).json({error: 'Name must be unique'})
@@ -94,35 +94,35 @@ app.post('/api/persons', (req, res) => {
   const personData = new mongoService ({
     name: body.name,
     number: body.number
-  });
+  })
 
   mongoService
-  .find({name: body.name})
-  .then(result => {
-    if (result.length > 0) {
-      res.send(405, 'Duplicate found, method Not Allowed');
-    }
+    .find({ name: body.name })
+    .then(result => {
+      if (result.length > 0) {
+        res.send(405, 'Duplicate found, method Not Allowed')
+      }
 
-    personData
-      .save()
-      .then(result => {
-        res.status(201).end();
+      personData
+        .save()
+        .then(result => {
+          res.status(201).end()
         })
 
-  })
+    })
 })
 
 app.get('/info', (req, res) => {
   mongoService
-  .find({}, {__v: 0})
-  .then(result => {
-    console.log('result', result);
-    const contactsAmount = result.length;
-    const date = Date();
-    res.send(`<h1>Puhelinluettelossa on ${contactsAmount} henkilön tiedot</h1>
-          <p>${date}</p>`);
+    .find({}, { __v: 0 })
+    .then(result => {
+      console.log('result', result)
+      const contactsAmount = result.length
+      const date = Date()
+      res.send(`<h1>Puhelinluettelossa on ${contactsAmount} henkilön tiedot</h1>
+          <p>${date}</p>`)
     })
-});
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
