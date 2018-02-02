590ef65b9bd5ecf18826b65929df364bf32f0a39
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-02 15:02:49 +0200

    process env

--- models/tietokanta.js
+++ models/tietokanta.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://admin:admin@ds223268.mlab.com:23268/koulukanta'
+// const url = 'mongodb://admin:admin@ds223268.mlab.com:23268/koulukanta'
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise;
